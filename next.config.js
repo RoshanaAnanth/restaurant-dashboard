697798f47ce962873b5382c9bd27bbd72f38c310
RoshanaAnanth <rosyrosh01@gmail.com>
2025-09-25 21:38:45 +0530

    feat: install gh-pages

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: { unoptimized: true },
-};
+const repo = process.env.GITHUB_REPOSITORY?.split("/")[1] || "your-repo";
 
-module.exports = nextConfig;
+module.exports = {
+  output: "export", // instruct Next to produce static HTML via next export
+  assetPrefix: `/${repo}/`, // ensure static assets load from /repo/
+  basePath: `/${repo}`, // makes routes resolve under /repo
+  trailingSlash: true, // helpful for GitHub Pages to serve index.html in folders
+};
